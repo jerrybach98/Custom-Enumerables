811ee9b17cd2aff29907576c354ac13ea36a3004
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2023-09-13 00:20:09 -0700

    custom enumerable methods

---
+++ my_enumerables.rb
@@ -0,0 +1,78 @@
+module Enumerable
+  def my_each
+    for element in self
+      yield element
+    end
+  end
+
+  def my_each_with_index
+    index = 0
+    for element in self
+      yield element, index
+      index += 1
+    end
+  end
+
+
+  def my_select
+    result = []
+    for element in self 
+      result << element if yield element
+    end
+    result
+  end
+
+
+  def my_all?
+    for element in self 
+      return false unless yield element
+    end
+    true
+  end
+
+  def my_any?
+    for element in self 
+      return true if yield element
+    end
+    false
+  end
+
+  def my_none?
+    for element in self 
+      return false if yield element
+    end
+    true
+  end
+
+  def my_count
+    counter = 0
+    if block_given?
+      for element in self
+      counter += 1 if yield element
+      end
+    else
+      for element in self
+        counter += 1
+      end
+    end
+    counter
+  end
+
+  def my_map
+    result = []
+    for element in self 
+      result << yield(element)
+    end
+    result
+  end
+
+  def my_inject (result = nil)
+    for element in self 
+      result = yield result, element
+    end
+    result
+  end
+
+
+end
+
